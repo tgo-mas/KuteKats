67715a8f0abe49305d963e4a98611905600e5330
Thomas Almeida <99335340+tgo-mas@users.noreply.github.com>
2023-02-16 14:29:01 -0300

    Adicionado novo alider

--- js/navbar-script.js
+++ js/navbar-script.js
@@ -17,29 +17,3 @@ function allEventListener() {
         })
     }
 }
-
-const swiper = new Swiper('.swiper', {
-
-    direction: 'horizontal',
-    loop: false,
-    slidesPerView: 1,
-    spaceBetween: 10,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    scrollbar: {
-        el: '.scrollbar'
-    },
-
-    breakpoints: {
-        '@0.75':{
-            slidesPerView: 2
-        },
-        '@1.00':{
-            slidesPerView: 3
-        }
-    }
-
-})
